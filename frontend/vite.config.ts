a8bbf4a798a7d6ef935a72b56078b859168a8231
Valentin <valesolorzanoss@gmail.com>
2026-05-30 19:43:49 -0600

    adicion de dockerfile para frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      host: env.VITE_HOST || '0.0.0.0',
+      port: Number(env.VITE_PORT) || 5173,
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
     }
   }
-
 })
